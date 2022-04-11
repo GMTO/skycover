739d8e0b0097c27af0f217a72697c50cf0731737
Bo Xin <bxin@gmto.org>
2022-04-10 20:51:22 -0700

    add guidestarselection from Marcos

--- agwsprobes.py
+++ agwsprobes.py
@@ -1,15 +1,16 @@
-## assign a probe to each guide star if possible and return "True"
-## otherwise, return "False"
+## assign a probe to each guide star if possible using the agwsvalid tools
+## Marcos van Dam, Flat Wavefronts, February 202
 
 import agwsvalid
 import numpy as np
 import sys
 import itertools
+import math
 
-def agwsprobes(obsmode,gsloc):
-    # gsloc is currently a 4x2 matrix of guide star positions
-    # will be upgraded to accept fewer guide stars
-    # obsmode is the observing mode, 'dgnf', 'm3', 'dgwf' 
+def nCk(n,k):
+    return np.int(math.factorial(n)/(math.factorial(n-k)*math.factorial(k)))
+
+def agwsinit(obsmode,silent=True):
 
     obsmode = obsmode.lower()
     
@@ -17,81 +18,154 @@ def agwsprobes(obsmode,gsloc):
         print('*** obsmode must be one of dgnf, m3 or dgwf ***')
         return (False,gsloc)
 
+    validator = agwsvalid.validator(obsmode,silent=silent)
+    validator.obsmode = obsmode
+    
+    return validator
+
+def agwsreachstars(validator,gspos):
+
+    nstars = (gspos.shape)[0]
+    results = np.zeros((4,nstars),dtype=bool)
+
+    if validator.obsmode == 'dgnf':
+        startloc = np.array([[0,0.166],[-0.166,0],[0,-0.166],[0.166,0]])*3600.
+    if validator.obsmode == 'm3':
+        startloc = 0.166*np.array([[np.sin(0.2),np.cos(0.2)],[-1,0.],[0,-1],[1,0]])*3600
+    if validator.obsmode == 'dgwf':
+        startloc = np.array([[0,0.159],[-0.159,0],[0,-0.159],[0.159,0]])*3600.
+
+    for ns in range(nstars):
+        for k in range(4):
+            loc = startloc.copy()
+            loc[k,:] = gspos[ns,:]
+            t = np.ndarray.flatten(loc / 3600.)
+            results[k,ns] = validator.check(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7])
+
+    return results
+
+def agwscheck(validator,gsloc,nstars_out=4):
+    # gsloc is currently a 4x2 matrix of guide star positions
+    # will be upgraded to accept fewer guide stars
+    # obsmode is the observing mode, 'dgnf', 'm3', 'dgwf' 
+    
     dims = gsloc.shape
     if len(dims) != 2:
         print('*** gsloc must be an Nx2 array of star coordinates ***')
-        return (False,gsloc)
+        return (False,gsloc,[])
 
     if dims[1] != 2:
         print('*** gsloc must be an Nx2 array of star coordinates ***')
-        return (False,gsloc)
+        return (False,gsloc,[])
 
     nstars = dims[0]
-    
-    if nstars > 4:
-        print('*** gsloc must have 4 or fewer stars ***')
-        return (False,gsloc)
-    
-    validator = agwsvalid.validator(obsmode,silent=True)
+        
+    if validator.obsmode == 'dgnf':
+        startloc = np.array([[0,0.166],[-0.166,0],[0,-0.166],[0.166,0]])*3600.
+    if validator.obsmode == 'm3':
+        startloc = 0.166*np.array([[np.sin(0.2),np.cos(0.2)],[-1,0.],[0,-1],[1,0]])*3600
+    if validator.obsmode == 'dgwf':
+        startloc = np.array([[0,0.159],[-0.159,0],[0,-0.159],[0.159,0]])*3600.
 
-    if obsmode == 'dgnf':
-        startloc = np.array([[0,0.166],[-0.166,0],[0,-0.166],[0.166,0]])
-    if obsmode == 'm3':
-        startloc = 0.166*np.array([[np.sin(0.2),np.cos(0.2)],[-1,0.],[0,-1],[1,0]])
-    if obsmode == 'dgwf':
-        startloc = np.array([[0,0.159],[-0.159,0],[0,-0.159],[0.159,0]])
+    if nstars_out < 4:
+        vals = []
+        idx = []
+
+        # here, we input 4 or more stars but return fewer than 4 valid stars
+        gslocs = np.concatenate((gsloc,startloc),axis=0)
+
+        comb1 = list(itertools.combinations(range(0,nstars),nstars_out))
+        comb2 = list(itertools.combinations(range(nstars,nstars+4),4-nstars_out))
+
+        combinations = list(itertools.product(*(comb1,comb2)))
+        
+        flatten = lambda l: [item for sublist in l for item in sublist]
+        for k in range(len(combinations)):
+            combinations[k] = flatten(combinations[k])
             
+        for comb in combinations:
+            for perm in list(itertools.permutations(comb)):
+                #import pdb;pdb.set_trace()
+
+                loc = gslocs[perm,:]
+                t = np.ndarray.flatten(loc / 3600.)
+                check = validator.check(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7])
+                if check:
+                    vals.append(loc)
+                    idx.append(np.array(perm))
+                    break
+
+        if vals == []:
+            return (False,gsloc,idx)
+        else:
+            return (True,vals,idx)
+                
     if nstars < 4:
         gslocs = np.concatenate((gsloc,startloc),axis=0) 
-        combinations = np.array(list(itertools.combinations([0,1,2,3,4],4)))
-
-        if nstars == 1:
-            ncomb = 4
-        if nstars == 2:
-            ncomb = 6
-        if nstars == 3:
-            ncomb = 4
-
+        
+        ncomb = nCk(4,nstars)
+        combinations = np.array(list(itertools.combinations(range(nstars+4),4)))
         combinations = combinations[0:ncomb,:] # only include the ones with the specified gsloc
-        permutations = np.array(list(itertools.permutations([0,1,2,3])))
+        
         for comb in combinations:
-            gsloc = gslocs[comb,:]
-            for perm in permutations:
-                loc = gsloc[perm,:]
-                t = np.ndarray.flatten(loc)
+            for perm in list(itertools.permutations(comb)):
+                loc = gslocs[perm,:]
+                t = np.ndarray.flatten(loc/3600.)
                 check = validator.check(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7])
                 if check:
                     gsloc = loc
-                    return (True,gsloc)
+                    return (True,gsloc,np.array(perm))
 
-        return (False,gsloc)
+        return (False,gsloc,[])
+
+    if nstars == 4:
+        permutations = np.array(list(itertools.permutations([0,1,2,3])))
+        for perm in permutations:
+            loc = gsloc[perm,:]
+            t = np.ndarray.flatten(loc / 3600.)
+            check = validator.check(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7])
+            if check:
+                gsloc = loc
+                return (True,gsloc,np.array(perm))
+
+        return (False,gsloc,[])
+
+    if nstars > 4:
+        vals = []
+        idx = []
+        combinations = np.array(list(itertools.combinations(range(nstars),4)))
+        for comb in combinations:
+            permutations = np.array(list(itertools.permutations(comb)))
+            for perm in permutations:
+                loc = gsloc[perm,:]
+                t = np.ndarray.flatten(loc / 3600.)
+                check = validator.check(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7])
+                if check:
+                    vals.append(loc)
+                    idx.append(np.array(perm))
+                    break
+
+        if vals == []:
+            return (False,gsloc,idx)
+        else:
+            return (True,vals[0],idx[0])
+
+def test():
+    config  = 'dgnf'        
+    validator = agwsinit(config)
     
-    permutations = np.array(list(itertools.permutations([0,1,2,3])))
-    for perm in permutations:
-        loc = gsloc[perm,:]
-        t = np.ndarray.flatten(loc)
-        check = validator.check(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7])
-        if check:
-            gsloc = loc
-            return (True,gsloc)
-
-    return (False,gsloc)
-
-## let us try an example with four guide stars and let the software decide on the order
-
-dist = 0.159
-
-gsloc = np.array([[0,dist],[-dist,0],[0,-dist],[dist,0]])
-gsloc = gsloc[[3,2,0,1],:]
-
-(success,loc) = agwsprobes('dgnf',gsloc)
-print(success)
-
-gs2loc = gsloc[0:2,:]
-(success,loc) = agwsprobes('m3',gs2loc)
-print(success)
-
-gs1loc = np.array([[0.1,0.1]])
-(success,loc) = agwsprobes('dgwf',gs1loc)
-print(success)
-
+    gsloc = np.array([[0,0.166],[-0.166,0],[0,-0.166],[0.166,0]])*3600
+    (success,gspos,idx) = agwscheck(validator,gsloc)
+    print(success,idx)
+    
+    
+    gsloc = np.array([[0,-0.166]])*3600
+    (success,gspos,idx) = agwscheck(validator,gsloc)
+    print(success,idx)
+    
+    gsloc = np.array([[0,0.166],[0.14,0],[0,-0.166],[0.166,0]])*3600
+    (success,gspos,idx) = agwscheck(validator,gsloc)
+    print(success,idx)
+    
+    (success,gspos,idx) = agwscheck(validator,gsloc,nstars_out=3)
+    print(success,idx)
